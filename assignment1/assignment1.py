b68731acbbf0bfba22cf36ce4fa01bac8598213c
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2025-11-15 23:22:03 -0600

    Task2 and Task3

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,2 +1,28 @@
 def hello():
   return "Hello!"
+
+def greet(name):
+  return f"Hello, {name}!"
+
+def calc(val1, val2, operator = "multiply"):
+  match operator:
+    case "add":
+      return val1 + val2
+    case "subtract":
+      return val1 - val2
+    case "multiply":
+      try:
+        int(val1)
+      except:
+        return "You can't multiply those values!"
+      else:
+        return val1 * val2
+    case "divide":
+      try:
+        val1 / val2
+      except ZeroDivisionError:
+        return "You can't divide by 0!"
+      else:
+        return val1 / val2
+    case "modulo":
+      return val1 % val2
