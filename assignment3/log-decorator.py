dbbcf589978a94495e16a77bfe235a6b9684d5a9
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2025-12-07 16:19:47 -0600

    Task 1

---
+++ assignment3/log-decorator.py
@@ -0,0 +1,32 @@
+import logging
+
+logger = logging.getLogger(__name__ + "_parameter_log")
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.FileHandler("./decorator.log", "a"))
+
+def logger_decorator(func):
+  def wrapper(*args, **kwargs):
+    logger.info(f"function {func.__name__} was called")
+    return func(*args, **kwargs)
+  return wrapper
+  
+@logger_decorator
+def say_hello():
+    print("Hello World!")
+
+
+@logger_decorator
+def always_true(*args):
+    print(*args, "*args")
+    return True
+
+
+@logger_decorator
+def return_decorator(**kwargs):
+    print(kwargs, "**kwargs")
+    return logger_decorator
+
+if __name__ == "__main__":
+  say_hello()
+  always_true(1, 2, 3, "test")
+  return_decorator(name="Dmytro", age=30)
