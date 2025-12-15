5742ff6f9760a121e1cec271623163c7b54bb53d
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2025-12-14 20:03:34 -0600

    Task 2

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# Task 1
+
 task1_data_frame = pd.DataFrame({
   "Name": ['Alice', 'Bob', 'Charlie'],
   "Age": [25, 30, 35],
@@ -14,4 +16,11 @@ task1_older = task1_with_salary.copy()
 task1_older["Age"] += 1
 
 task1_older.to_csv("employees.csv", index=False)
-print(task1_older)
+
+# Task 2
+task2_employees = pd.read_csv("employees.csv")
+
+json_employees = pd.read_json("./additional_employees.json")
+
+more_employees = pd.concat([task2_employees, json_employees], ignore_index=True)
+print(more_employees)
