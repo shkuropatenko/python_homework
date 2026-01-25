67ac5dc589669edef23bc15fbfc898dfffaaca0a
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2026-01-24 19:34:01 -0600

    Task 2

--- assignment9/advanced_sql.py
+++ assignment9/advanced_sql.py
@@ -7,13 +7,6 @@ try:
         cursor = conn.cursor()
         print("Connected to DB:", DB_PATH)
 
-        # cursor.execute("""
-        #   SELECT order_id,
-        #   FROM orders
-        #   JOIN Students ON Enrollments.student_id = Students.student_id
-        #   JOIN Courses ON Enrollments.course_id = Courses.course_id;
-        # """)
-
         print("\n--- total price of each of the first 5 orders ---")
         
         cursor.execute("""
@@ -28,6 +21,29 @@ try:
         
         for row in cursor.fetchall():
             print(row)
+
+        print("\n--- AVG price of each of the first 5 orders ---")
+        cursor.execute("""
+          SELECT
+              c.customer_name,
+              AVG(t.total_price) AS average_total_price
+          FROM customers c
+          LEFT JOIN (
+              SELECT
+                  o.customer_id AS customer_id_b,
+                  SUM(p.price * li.quantity) AS total_price
+              FROM orders o
+              JOIN line_items li ON o.order_id = li.order_id
+              JOIN products p ON li.product_id = p.product_id
+              GROUP BY o.order_id
+          ) t ON c.customer_id = t.customer_id_b
+          GROUP BY c.customer_id
+          LIMIT 5;
+      """)
+
+        for row in cursor.fetchall():
+            print(row)
+
 
 
 
